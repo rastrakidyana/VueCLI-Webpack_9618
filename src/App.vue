30383de25636ded8944e177ed75c3b2f1be1669e
rastrakidyana <rastrakidyana@users.noreply.github.com>
2020-11-08 18:35:28 +0800

    GD Vue CLI

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <router-view></router-view>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  export default {
+    name: "App",
+  };
 </script>
+
 
 <style>
 #app {
